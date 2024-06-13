55e92e205b274646ab60d6bf663758d533ff2a52
paveltspavlov <pavel_ts_pavlov@outlook.com>
2024-06-13 09:02:56 +0300

    Homework4 - task 4 WiP

--- homework4/clever_lily.py
+++ homework4/clever_lily.py
@@ -3,29 +3,31 @@ washer_price = float(input())
 toy_price = int(input())
 
 money_saved = 0
-toys = 1
-money_taken = 0
+money_saved1 = 0
+toys = 0
+money_taken = 1
 i = 0
 buff = 0
+buff2 = 0
 
-while i < lily_age:
+for i in range(i, lily_age):
 
     if i % 2 == 0:
-        buff += 1
-        money_saved += buff*10
-        money_taken += 1
-
+        buff += 10
+#        money_saved += buff
+        money_saved += buff
+        money_taken += buff2
+        print(money_saved)
+        print(money_taken)
+        print("__________")
+        buff2 =+ 1
     else:
         toys += 1
 
     i += 1
 
-
 toys_total = toys * toy_price
-print(money_taken)
-print(money_saved)
-money_saved_total = (money_saved + toys_total) - money_taken
-print(money_saved_total)
+money_saved_total = money_saved + toys_total - money_taken
 
 if money_saved_total >= washer_price:
     diff = float(money_saved_total - washer_price)
