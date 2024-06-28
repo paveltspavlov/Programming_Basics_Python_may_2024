3140681532fdd694a014dd235d0117724f16a753
paveltspavlov <pavel_ts_pavlov@outlook.com>
2024-06-28 18:19:05 +0300

    Homework 5 task 2 done

--- homework5/exam_prep.py
+++ homework5/exam_prep.py
@@ -2,21 +2,31 @@ bad_grades = int(input())
 bad_grades_count = 0
 avg_score = 0
 total_score = 0
+total_grades = 0
 exam_name = ""
+last_problem = ""
 
 while exam_name != "Enough":
     exam_name = input()
-    grade = int(input())
+
     if exam_name == "Enough":
         break
-    elif grade <= 4:
+    grade = int(input())
+
+    if grade <= 4:
         bad_grades_count += 1
         if bad_grades_count == bad_grades:
-            print(f"You need a break, {bad_grades} poor grades.")
             break
-        continue
-    else:
-        avg_score += grade
 
-print(f"Average score: {avg_score / grade}")
-print(f"Last problem: {exam_name}")
+    total_score += grade
+    last_problem = exam_name
+    total_grades += 1
+
+average = total_score / total_grades
+
+if bad_grades_count < bad_grades:
+    print(f"Average score: {average:.2f}")
+    print(f"Number of problems: {total_grades}")
+    print(f"Last problem: {last_problem}")
+else:
+    print(f"You need a break, {bad_grades} poor grades.")
